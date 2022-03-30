1631d53e4b2558d859615e7096f355464acba494
Brandon Miller <brandon.miller@hashicorp.com>
2022-03-30 15:14:11 +0000

    Path for S3 Module Updated

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,5 @@
 module "s3-bucket" {
-  source  = "app.terraform.io/instruqt-circleci/s3-bucket/aws"
+  source  = "app.terraform.io/bm_mi_lab/s3-bucket/aws"
   version = "~>4.7.0"
   bucket_prefix = var.prefix
 }
