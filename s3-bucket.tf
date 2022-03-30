594094c3128b31672c786ebf2ae9e37315b72b6b
Brandon Miller <brandon.miller@hashicorp.com>
2022-03-30 15:32:05 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,7 @@
 module "s3-bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
-  version = "3.0.0"
-  
+  version = "~>3.0.0"
+
   # insert required variables here
   bucket_prefix = var.prefix
 }
